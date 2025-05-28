6fb2476d821f87a5151d682b70cdcde5b0b8a428
Manal137 <manal.saheer@biztras.com>
2025-05-29 00:44:58 +0530

    frontend

--- src/App.js
+++ src/App.js
@@ -51,15 +51,63 @@
 // export default App;
 
 
-
-// src/pages/PrivateRoute.js
+// src/App.js
 import React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-const PrivateRoute = () => {
-  const user = JSON.parse(localStorage.getItem('user'));
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+import DashboardPage from './pages/DashboardPage';
+import AdminPage from './pages/AdminPage';
+import AdminLoginPage from './pages/AdminLoginPage';
+import AdminSetupPage from './pages/AdminSetupPage';
+import LandingPage from './pages/LandingPage';
 
-  return user ? <Outlet /> : <Navigate to="/login" replace />;
-};
+import PrivateRoute from './pages/PrivateRoute';
+import AdminRoute from './pages/AdminRoute';
+import { Outlet } from 'react-router-dom';
 
-export default PrivateRoute;
+const router = createBrowserRouter([
+  // 🔓 Public Routes
+  { path: '/', element: <HomePage /> },
+  { path: '/login', element: <LoginPage /> },
+  { path: '/register', element: <SignupPage /> },
+  { path: '/admin-login', element: <AdminLoginPage /> },
+  { path: '/admin-setup', element: <AdminSetupPage /> },
+
+  // 🔐 Protected User Routes
+  {
+    path: '/',
+    element: <PrivateRoute />,
+    children: [
+      {
+        path: '',
+        element: <Outlet />,
+        children: [
+          { path: 'dashboard', element: <DashboardPage /> },
+          { path: 'landing', element: <LandingPage /> },
+        ],
+      },
+    ],
+  },
+
+  // 🔐 Protected Admin Route
+  {
+    path: '/',
+    element: <AdminRoute />,
+    children: [
+      {
+        path: '',
+        element: <Outlet />,
+        children: [{ path: 'admin', element: <AdminPage /> }],
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
